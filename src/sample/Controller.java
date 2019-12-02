deb1320c3fb7db5aec337cbf60fee77ece088842
Levi Bare <levi.bare@gmail.com>
2019-12-02 11:32:58 -0800

    Shows combo box

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -216,6 +216,7 @@ public class Controller implements Initializable {
             map = a.getMap();
             searchResults = FXCollections.observableArrayList(a.getLocationResults());
             resultsBox.setItems(searchResults);
+            resultsBox.show();
         }
     }
 
