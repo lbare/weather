98814eaaef903f79502eca35423cc77de55dad3c
Levi Bare <levi.bare@gmail.com>
2019-12-06 14:09:55 -0800

    Added toggle C/F button to work in background

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -35,18 +35,21 @@ public class Controller implements Initializable {
     private boolean tempState;
     private HashMap<String,String> map;
     private String selection;
-    private Image Icon1, day0icon, day1icon, day2icon, day3icon, day4icon, day5icon, day6icon;;
+    private Image Icon1, day0icon, day1icon, day2icon, day3icon, day4icon, day5icon, day6icon;
+    private Weather w1;
 
     ObservableList<String> searchResults = FXCollections.observableArrayList("");
 
     public void handleGoButton(){
         AsyncTask t = new locationSearchBackground();
         t.execute("test");
+        buttonClicked = 0;
     }
 
     public void myLocation(){
         AsyncTask t = new myLocationBackground();
         t.execute("test");
+        buttonClicked = 1;
     }
 
     public void autoFillUpdate(KeyEvent e){
@@ -54,6 +57,12 @@ public class Controller implements Initializable {
             AsyncTask t = new autofillBackground();
             t.execute("test");
         }
+    }
+
+    public void toggleButton(){
+        AsyncTask a = new toggleBackground();
+        a.execute("test");
+        tempState = !tempState;
     }
 
     public void clearSearch() {
@@ -74,78 +83,6 @@ public class Controller implements Initializable {
         weatherLabel.setVisible(true);
         locationLabel.setVisible(true);
         imageView.setVisible(true);
-
-    }
-
-    public void toggleButton(){
-        if (buttonClicked == 0) {
-            selection = map.get(resultsBox.getValue());
-            Weather w = new Weather(selection);
-            if (tempState) {
-                tempLabel.setText(w.getTemperatureF());
-                day0temp.setText(w.getAvgTempF(0));
-                day1temp.setText(w.getAvgTempF(1));
-                day2temp.setText(w.getAvgTempF(2));
-                day3temp.setText(w.getAvgTempF(3));
-                day4temp.setText(w.getAvgTempF(4));
-                day5temp.setText(w.getAvgTempF(5));
-                day6temp.setText(w.getAvgTempF(6));
-                maxLabel.setText(w.getMaxF());
-                minLabel.setText(w.getMinF());
-                windLabel.setText(w.getWindSpeedMPH());
-                visibilityLabel.setText(w.getVisibilityMI());
-                tempImage.setImage(new Image("file:icons/fah.png"));
-            }
-            else {
-                tempLabel.setText(w.getTemperatureC());
-                day0temp.setText(w.getAvgTempC(0));
-                day1temp.setText(w.getAvgTempC(1));
-                day2temp.setText(w.getAvgTempC(2));
-                day3temp.setText(w.getAvgTempC(3));
-                day4temp.setText(w.getAvgTempC(4));
-                day5temp.setText(w.getAvgTempC(5));
-                day6temp.setText(w.getAvgTempC(6));
-                maxLabel.setText(w.getMaxC());
-                minLabel.setText(w.getMinC());
-                windLabel.setText(w.getWindSpeedKPH());
-                visibilityLabel.setText(w.getVisibilityKM());
-                tempImage.setImage(new Image("file:icons/cel.png"));
-            }
-        }
-        else if (buttonClicked == 1) {
-            Weather w = new Weather();
-            if (tempState) {
-                tempLabel.setText(w.getTemperatureF());
-                day0temp.setText(w.getAvgTempF(0));
-                day1temp.setText(w.getAvgTempF(1));
-                day2temp.setText(w.getAvgTempF(2));
-                day3temp.setText(w.getAvgTempF(3));
-                day4temp.setText(w.getAvgTempF(4));
-                day5temp.setText(w.getAvgTempF(5));
-                day6temp.setText(w.getAvgTempF(6));
-                maxLabel.setText(w.getMaxF());
-                minLabel.setText(w.getMinF());
-                windLabel.setText(w.getWindSpeedMPH());
-                visibilityLabel.setText(w.getVisibilityMI());
-                tempImage.setImage(new Image("file:icons/fah.png"));
-            }
-            else {
-                tempLabel.setText(w.getTemperatureC());
-                day0temp.setText(w.getAvgTempC(0));
-                day1temp.setText(w.getAvgTempC(1));
-                day2temp.setText(w.getAvgTempC(2));
-                day3temp.setText(w.getAvgTempC(3));
-                day4temp.setText(w.getAvgTempC(4));
-                day5temp.setText(w.getAvgTempC(5));
-                day6temp.setText(w.getAvgTempC(6));
-                maxLabel.setText(w.getMaxC());
-                minLabel.setText(w.getMinC());
-                windLabel.setText(w.getWindSpeedKPH());
-                visibilityLabel.setText(w.getVisibilityKM());
-                tempImage.setImage(new Image("file:icons/cel.png"));
-            }
-        }
-        tempState = !tempState;
     }
 
     public class locationSearchBackground extends AsyncTask<String, Weather>{
@@ -180,6 +117,58 @@ public class Controller implements Initializable {
             searchResults = FXCollections.observableArrayList(a.getLocationResultsArray());
             resultsBox.setItems(searchResults);
             resultsBox.show();
+        }
+    }
+
+    public class toggleBackground extends AsyncTask<String, Weather>{
+        public Weather doInBackground(String query){
+            if (buttonClicked == 0) {
+                w1 = new Weather();
+            }
+            else {
+                w1 = new Weather();
+            }
+            return w1;
+        }
+        public void onPostExecute(Weather w){
+            toggleToC(w1);
+            toggleToF(w1);
+        }
+    }
+
+    public void toggleToF(Weather w){
+        if (tempState) {
+            tempLabel.setText(w.getTemperatureF());
+            day0temp.setText(w.getAvgTempF(0));
+            day1temp.setText(w.getAvgTempF(1));
+            day2temp.setText(w.getAvgTempF(2));
+            day3temp.setText(w.getAvgTempF(3));
+            day4temp.setText(w.getAvgTempF(4));
+            day5temp.setText(w.getAvgTempF(5));
+            day6temp.setText(w.getAvgTempF(6));
+            maxLabel.setText(w.getMaxF());
+            minLabel.setText(w.getMinF());
+            windLabel.setText(w.getWindSpeedMPH());
+            visibilityLabel.setText(w.getVisibilityMI());
+            tempImage.setImage(new Image("file:icons/fah.png"));
+        }
+    }
+
+    public void toggleToC(Weather w){
+        if (!tempState) {
+            tempLabel.setText(w.getTemperatureC());
+            day0temp.setText(w.getAvgTempC(0));
+            day1temp.setText(w.getAvgTempC(1));
+            day2temp.setText(w.getAvgTempC(2));
+            day3temp.setText(w.getAvgTempC(3));
+            day4temp.setText(w.getAvgTempC(4));
+            day5temp.setText(w.getAvgTempC(5));
+            day6temp.setText(w.getAvgTempC(6));
+            maxLabel.setText(w.getMaxC());
+            minLabel.setText(w.getMinC());
+            windLabel.setText(w.getWindSpeedKPH());
+            visibilityLabel.setText(w.getVisibilityKM());
+            tempImage.setImage(new Image("file:icons/cel.png"));
         }
     }
 
@@ -258,6 +247,11 @@ public class Controller implements Initializable {
         currentDateLabel.setText(w.getDate(0));
     }
 
+    public void displayOnStart(){
+        Weather w = new Weather();
+        displayInfo(w);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         resultsBox.setValue("");
@@ -269,7 +263,7 @@ public class Controller implements Initializable {
         tempImage.setImage(temp);
         arrowImage.setImage(arrow);
         bgImage.setImage(bg);
-        myLocation();
+        displayOnStart();
     }
 }
 
