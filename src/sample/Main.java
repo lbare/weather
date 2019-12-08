06bea9a762692a3d7d6d7104bd13ed0f2f8a9fdc
Levi Bare <levi.bare@gmail.com>
2019-12-08 14:24:14 -0800

    testing fixing location display

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Weather");
+        primaryStage.setTitle("");
         primaryStage.setScene(new Scene(root, 400, 675));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
